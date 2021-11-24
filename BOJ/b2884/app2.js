115d4e77089d2575e9ef6c0a20252c1c5e56a913
leedawnn <2g_hee@naver.com>
2021-11-24 15:15:32 +0900

    [BOJ] 2884번 문제풀이

---
+++ BOJ/b2884/app2.js
@@ -0,0 +1,19 @@
+// 이렇게도 풀 수 있음
+
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let hour = Number(input[0].split(" ")[0]);
+let minute = Number(input[0].split(" ")[1]);
+
+if (minute >= 45) {
+  minute -= 45;
+} else {
+  hour -= 1;
+  minute += 15;
+}
+if (hour === -1) {
+  hour = 23;
+}
+
+console.log(hour, minute);
