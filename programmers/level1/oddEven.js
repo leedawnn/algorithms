d9cadc8e1243d343d733741b4ec15b0b78632134
leedawnn <2g_hee@naver.com>
2022-07-12 16:55:56 +0900

    [프로그래머스] 짝수와 홀수 풀이

---
+++ programmers/level1/oddEven.js
@@ -0,0 +1,15 @@
+function solution(num) {
+	let answer;
+
+	if (num % 2 === 0) {
+		answer = 'Even';
+	} else {
+		answer = 'Odd';
+	}
+	return answer;
+}
+
+// 다른 풀이
+function evenOrOdd(num) {
+	return num % 2 ? 'Odd' : 'Even';
+}
