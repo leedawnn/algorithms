e640d8ca9253c58c1be1e4e297752936d1c24d79
leedawnn <2g_hee@naver.com>
2023-03-10 21:40:01 +0900

    [백준] ATM 풀이

---
+++ BOJ/b11399/app.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+
+const order = input[1].split(' ').map(Number);
+const newOrder = order.sort((a, b) => a - b); // [1, 2, 3, 3, 4]
+let temp = 0;
+let answer = 0;
+
+for (let i = 0; i < newOrder.length; i++) {
+  temp += newOrder[i];
+  if (temp > 0) {
+    answer += temp;
+  } else {
+    answer += newOrder[i];
+  }
+}
+
+console.log(answer);
