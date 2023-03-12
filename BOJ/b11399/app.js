21efa2766124c69a04bf00c75724ff701c27fdda
leedawnn <2g_hee@naver.com>
2023-03-12 18:10:53 +0900

    [백준] ATM 다르게 풀이

--- BOJ/b11399/app.js
+++ BOJ/b11399/app.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-// let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
 const order = input[1].split(' ').map(Number);
 const newOrder = order.sort((a, b) => a - b); // [1, 2, 3, 3, 4]
@@ -9,10 +9,8 @@ let answer = 0;
 
 for (let i = 0; i < newOrder.length; i++) {
   temp += newOrder[i];
-  if (temp > 0) {
+  if (temp) {
     answer += temp;
-  } else {
-    answer += newOrder[i];
   }
 }
 
