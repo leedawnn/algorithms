47c6069531bcf31bade3fc21f5c43c4d7b8865a1
leedawnn <dev.leedawn@gmail.com>
2024-03-19 16:18:03 +0900

    [BOJ] 2810번 컵홀더 문제 풀이

--- BOJ/b2810/app.js
+++ BOJ/b2810/app.js
@@ -1,38 +1,12 @@
-const input = require('fs')
+const [n, seat] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
   .split('\n');
 
-function solution(input) {
-  const seat = input[1].split('');
-  let stack = [];
-  let answer = 0;
+function solution(n, seat) {
+  const cupHolder = seat.split(/S|LL/g).length;
 
-  for (let i = 0; i < seat.length; i++) {
-    if (i === 0) stack.push('*');
-
-    if (seat[i] === 'S' && seat[i + 1] !== undefined) {
-      stack.push('S');
-      stack.push('*');
-    } else if (seat[i] === 'L' && seat[i + 1] !== undefined) {
-      stack.push('L');
-      stack.push('L');
-      stack.push('*');
-
-      i++;
-    } else {
-      stack.push(seat[i]);
-    }
-
-    if (i === seat.length) stack.push('*');
-  }
-
-  stack.forEach((el) => {
-    if (el === '*') answer++;
-  });
-
-  console.log(answer);
+  console.log(Math.min(n, cupHolder));
 }
-
-solution(input);
+solution(n, seat);
