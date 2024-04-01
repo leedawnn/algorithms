a5774e6b24cb3d3a83face3f41f1f05578670da3
leedawnn <dev.leedawn@gmail.com>
2024-04-01 13:28:26 +0900

    [BOJ] 11866번 요세푸스 문제 0 풀이

--- BOJ/b11866/app.js
+++ BOJ/b11866/app.js
@@ -5,39 +5,65 @@ const input = require('fs')
   .split(' ')
   .map(Number);
 
-let queue = [];
+// TODO: 복습!
 
-for (let i = 1; i <= input[0]; i++) {
-  queue.push(i);
-}
+// 나의 풀이,, 7 6 반례를 통과하지 못함ㅠ
 
-function solution(input, queue) {
-  let count = 0;
-  let temp = [];
-  let answer = [];
+// function solution() {
+//   const [n, k] = input;
+//   const arr = Array.from({ length: n }, (_, i) => i + 1);
 
-  for (let i = 0; i < input[1]; i++) {
-    if (count === input[1]) {
-      answer.push(...queue);
-      break;
-    }
+//   let answer = [];
 
-    temp.push(queue.slice(0, input[1] + 1));
+//   for (let i = 0; i < n; i++) {
+//     let temp = [];
+//     let first = arr.slice(0, k);
 
-    console.log(temp);
-    queue.splice(0, input[1] + 1);
+//     temp = first;
 
-    temp = [];
-    // for (let j = 0; j < input[1]; j++) {
-    //   temp.push(queue.shift());
-    //   answer.push(temp.pop());
-    //   count++;
+//     let lastElement = temp.pop();
+//     let rest = arr.slice(0, k - 1);
 
-    //   temp = [];
-    // }
+//     if (arr.length > k) {
+//       answer.push(lastElement);
+
+//       for (let i = 0; i < k; i++) {
+//         arr.shift();
+//       }
+
+//       rest.forEach((el) => {
+//         arr.push(el);
+//       });
+//     } else {
+//       answer.push(lastElement);
+//       rest.forEach((el) => answer.push(el));
+
+//       break;
+//     }
+//   }
+
+//   console.log(`<${answer.join(', ')}>`);
+// }
+
+// solution(input);
+
+// 다른 풀이...
+function solution() {
+  const [n, k] = input;
+  const arr = Array.from({ length: n }, (_, i) => i + 1);
+
+  let answer = []; // 제거된 사람들을 저장할 배열
+  let idx = 0; // 현재 위치를 추적할 인덱스
+
+  while (arr.length > 0) {
+    // 모든 사람이 제거될 때까지 반복
+    idx = (idx + k - 1) % arr.length; // K번째 사람의 인덱스를 계산 (배열은 0부터 시작하므로 k-1을 더함)
+    answer.push(arr.splice(idx, 1)[0]); // K번째 사람을 배열에서 제거하고, 제거된 사람을 answer에 추가
+
+    console.log(arr);
   }
 
-  // console.log(`<${answer.join(', ')}>`);
+  console.log(`<${answer.join(', ')}>`);
 }
 
-solution(input, queue);
+solution(input);
