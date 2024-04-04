20d8b7a96cbfa198f0d05ae760eaaa50798c12a9
leedawnn <dev.leedawn@gmail.com>
2024-04-04 16:36:10 +0900

    [Programmers] level0 배열 조각 하기 풀이

---
+++ programmers/level0/python/배열조각하기.py
@@ -0,0 +1,21 @@
+# 나의 풀이
+def solution(arr, query):
+    for i in range(len(query)):
+        index = query[i]
+        
+        if (i % 2 == 0):
+            arr = arr[:index+1]
+        else:
+            arr = arr[index:]
+
+    return arr
+
+# TODO: 복습!
+# enumerate를 활용한 풀이
+def solution(arr, query):
+    for k, q in enumerate(query):
+        if k % 2 == 0:
+            arr = arr[:q + 1]
+        else: 
+            arr = arr[q:]
+    return arr
